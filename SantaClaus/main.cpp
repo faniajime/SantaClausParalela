79f23f546a6668f97c35b563edf0d84cc14c184c
Fabiola Jiménez González <faniajime@gmail.com>
2019-09-21 16:01:42 -0600

    Initial Commit

---
+++ SantaClaus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SantaClaus
+//
+//  Created by Fabiola on 9/21/19.
+//  Copyright © 2019 Fabiola Jimenez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
